b7fc1087e047dd488cfe9b9bb78e5792a7b5b09e
tlhpaul55 <tlhpaul@hotmail.com>
2021-12-17 14:29:15 +0800

    changed out title

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
-st.title('Penguin Classifier')
-st.write("This app uses 6 inputs to predict the species of prenuin \
+st.title('Penguin Classifier: A Machine Learning App')
+st.write("This app uses 6 inputs to predict the species of penguin \
         using a model built on the Palmer's Penguin's dataset.  Use \
         the form below to get started!")
 
